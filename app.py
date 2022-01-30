7852c1b2f9ad70ddeeb7211db224b9662a0015f6
Kevin <kmeinon@gmail.com>
2022-01-30 23:07:54 +0100

    Fixed login only checking first DB entry

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def create_app():
     app.secret_key = os.environ.get("SECRET_KEY")
     app.permanent_session_lifetime = datetime.timedelta(days=30)
 
-
     @app.route("/")
     def root():
         return redirect(url_for("home"))
@@ -32,8 +31,8 @@ def create_app():
                 if user_id["username"] == request.form['user'] and user_id["password"] == request.form["password"]:
                     session["user"] = user_id["username"]
                     return redirect(url_for("user"))
-                else:
-                    return render_template("html/index.html")
+
+            return render_template("html/index.html")
         elif "user" in session:
             return redirect(url_for("user"))
         else:
@@ -62,27 +61,31 @@ def create_app():
         else:
             return redirect(url_for("home"))
 
-    @app.route("/user/recent/", methods=["GET"])
+    @app.route("/user/recent/", methods=["GET", "POST"])
     def recent():
-        name = session["user"]
-        page = request.args.get("page")
-        if page is not None:
-            page = int(page)
-        else:
-            page = 1
-        count = app.db[name].count_documents({})
-        page_count = 1
-        for x in range(count):
-            if (x + 1) % 10 == 0:
-                page_count += ((x+1) // 10)
-        entries_with_date = [
-            (entry["content"],
-             entry["date"],
-             datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%d %b")
-             )
-            for entry in app.db[name].find().skip((page - 1) * 10).limit(10 * page).sort("date", -1)
-        ]
-        return render_template("html/recent.html", entries=entries_with_date, links=page_count, page=page)
+        if request.method == "GET":
+            name = session["user"]
+            page = request.args.get("page")
+            if page is not None:
+                page = int(page)
+            else:
+                page = 1
+            count = app.db[name].count_documents({})
+            page_count = 1
+            for x in range(count):
+                if (x + 1) % 10 == 0:
+                    page_count += ((x+1) // 10)
+            entries_with_date = [
+                (entry["content"],
+                 entry["date"],
+                 datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%d %b"),
+                 entry["_id"]
+                 )
+                for entry in app.db[name].find().skip((page - 1) * 10).limit(10 * page).sort("date", -1)
+            ]
+            return render_template("html/recent.html", entries=entries_with_date, links=page_count, page=page)
+        elif request.method == "post":
+            return redirect(url_for("recent"))
 
     @app.route("/logout/")
     def logout():
