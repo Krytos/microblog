110c1c6a228af3e26341a12354c6392cdfcc7f37
Kevin <kmeinon@gmail.com>
2022-01-30 16:14:52 +0100

    Added session data + secret key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 from pymongo import MongoClient
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,12 +11,14 @@ def create_app():
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
+    app.secret_key = os.environ.get("SECRET_KEY")
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             username = request.form['user']
             password = request.form["password"]
+            session["user"] = username
             for user_id in app.db.login.find():
                 if user_id["username"] == username and user_id["password"] == password:
                     return redirect(url_for("user", name=username))
@@ -26,23 +28,25 @@ def create_app():
     @app.route("/<name>/", methods=["GET", "POST"])
     def user(name):
 
-        if request.method == "POST":
-            entry_content = request.form.get('content')
-            if entry_content is not None and len(entry_content) >= 2:
-                formatted_date = datetime.date.today().strftime("%Y-%m-%d")
-                app.db[name].insert_one({"content": entry_content, "date": formatted_date})
-            else:
-                pass
+        if "user" in session:
+            if request.method == "POST":
+                entry_content = request.form['content']
+                if entry_content is not None and len(entry_content) >= 2:
+                    formatted_date = datetime.date.today().strftime("%Y-%m-%d")
+                    app.db[name].insert_one({"content": entry_content, "date": formatted_date})
+                else:
+                    pass
 
-        entries_with_date = [
-            (entry["content"],
-             entry["date"],
-             datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%d %b")
-             )
-            for entry in app.db[name].find().limit(5)
-        ]
-
-        return render_template("html/home.html", entries=entries_with_date, name=name)
+            entries_with_date = [
+                (entry["content"],
+                 entry["date"],
+                 datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%d %b")
+                 )
+                for entry in app.db[name].find().limit(5)
+            ]
+            return render_template("html/home.html", entries=entries_with_date, name=name)
+        else:
+            return redirect(url_for("home"))
 
     @app.route("/<name>/recent/", methods=["GET"])
     def recent(name):
@@ -54,7 +58,6 @@ def create_app():
         count = app.db[name].count_documents({})
         page_count = 1
         for x in range(count):
-            # print(f"x = {x}")
             if (x + 1) % 10 == 0:
                 page_count += ((x+1) // 10)
         entries_with_date = [
@@ -65,14 +68,4 @@ def create_app():
             for entry in app.db[name].find().skip((page - 1) * 10).limit(10 * page)
         ]
         return render_template("html/recent.html", entries=entries_with_date, links=page_count, name=name, page=page)
-
-    # @app.route("/<name>/recent/<page_count>", metho=["GET"])
-    # def current_page(name, page_count):
-    #     count = app.db[name].count_documents({})
-    #     page_count = 1
-    #     for x in range(count):
-    #         print(f"x = {x}")
-    #         if (x + 1) % 10 == 0:
-    #             page_count = int(((x + 11) / 10))
-
     return app
